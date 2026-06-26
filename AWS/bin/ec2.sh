e534b20c71c586f5495bb897abb12f172c03c2ef
Reuben Pasquini <reuben@frickjack.com>
2026-06-26 16:59:16 -0500

    [#25]chore(dev-vpc): ec2 helpers and iam fixes
    - add ec2 helpers to start dev vm's
    - allow ec2 service to assume core littleware roles

--- AWS/bin/ec2.sh
+++ AWS/bin/ec2.sh
@@ -4,16 +4,50 @@ source "${LITTLE_HOME}/lib/bash/utils.sh"
 
 # lib -------------------------------
 
-public-ip() {
-    local instanceId="$1"
+vm-name2id() {
+    local name="$1"
+    local sshConfig="$2"
+    local instId
 
-    [[ -n "$instanceId" ]] || {
-        gen3_log_err "public-ip requires instanceId"
+    if [[ -z "$name" ]]; then
+        gen3_log_err "describe-vm requires ssh-config name or aws instance id"
         return 1
-    }
+    fi
+    if [[ "$name" =~ ^i- ]]; then
+        instId="$name"
+    else
+        local awkScript="$(cat - <<EOM
+/^[A-Z]/ { skipHost="true" }
+\$0 == "Host " name { skipHost="false" }
+skipHost=="false" { print; }
+EOM
+    )"
+        sshConfig="${sshConfig:-"$(cat "$HOME/.ssh/config")"}"
+        instId="$(
+            awk -v "name=$name" "$awkScript" <<< "$sshConfig" \
+            | awk -F : '/Tag ec2-instance-id/ { print $2 }'
+            )"
+    fi
+    if [[ ! "$instId" =~ ^i- ]]; then
+        gen3_log_err "unable to determine instance-id from name or ssh/config"
+        return 1
+    fi
+    echo "$instId"
+}
+
+vm-describe() {
+    aws ec2 describe-instances --output json --instance-ids "$(vm-name2id "$@")"
+}
+
+vm-public-ip() {
     local info
-    info="$(aws ec2 describe-instances --output json --instance-ids "$instanceId")" || return 1
-    cat - <<< "$info"
+    info="$(vm-describe "$@")" || return 1
+    jq --arg instId "$instanceId" -e -r '.Reservations[0].Instances[0] | .PublicIpAddress' <<< "$info"
+}
+
+vm-start() {
+    gen3_log_info "if state is stopped, then start the given vm"
+    aws ec2 start-instances --instance-ids "$(vm-name2id "$@")"
 }
 
 #
@@ -35,6 +69,30 @@ ssh-config-filter() {
     shift
     local currentConfig=""
     currentConfig="$(cat -)"
+
+    # Put the host at the top of the file,
+    # so Match blocks are below it
+    cat - <<EOM
+Host $name
+    HostName $publicIp
+    Tag little-trusted:true
+EOM
+    local tag
+    for tag in "$@"; do
+        echo "    Tag $tag"
+    done
+    echo ""
+
+    # remove existing config for Host $name
+    local awkScript="$(cat - <<EOM
+/^[A-Z]/ { skipHost="false" }
+\$0 == "Host " name { skipHost="true" }
+skipHost=="false" { print; }
+EOM
+    )"
+    awk -v "name=$name" "$awkScript" <<< "$currentConfig"
+    # add the new config
+    
     if [[ -z "$currentConfig" ]]; then
         gen3_log_err "ssh-config read empty config - add IdentityFile directive to little-trusted matching rule"
         cat - <<EOM
@@ -46,26 +104,21 @@ Match tagged little-trusted:true
 
 EOM
     fi
-    # remove existing config for Host $name
-    local awkScript="$(cat - <<EOM
-/^[A-Z]/ { skipHost="false" }
-\$0 == "Host " name { skipHost="true" }
-skipHost=="false" { print; }
-EOM
-    )"
-    awk -v "name=$name" "$awkScript" <<< "$currentConfig"
-    # add the new config
-    cat - <<EOM
+}
 
-Host $name
-    HostName: $publicIp
-    Tag little-trusted:true
-EOM
-    local tag
-    for tag in "$@"; do
-        echo "    Tag $tag"
-    done
-    echo ""
+vm-install-tools() {
+    local -a commands=(
+        "dnf check-update"
+        "dnf upgrade -y"
+        'dnf install docker'
+        'usermod -a -G docker ec2-user'
+        'dnf install java-25-amazon-corretto-devel'
+        'dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo'
+        "rpm --import https://packages.microsoft.com/keys/microsoft.asc"
+        'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+        'dnf install -y code'
+        'dnf install -y gh'
+    )
 }
 
 ssh-config-update() {
@@ -77,6 +130,9 @@ ssh-config-update() {
         gen3_log_err "ssh-config-update requires name argument"
         return 1
     }
+    if [[ -z "$instanceId" ]]; then
+        instanceId="$(vm-name2id "$name")"
+    fi
     [[ -n "$instanceId" ]] || {
         gen3_log_err "ssh-config-update requires instanceId argument"
         return 1
@@ -88,10 +144,32 @@ ssh-config-update() {
 
     local ip
     local newConfig
-    cp "$sshPath" "${sshPath}.bak" \
-    && ip="$(little ec2 public-ip $instanceId)" \
-    && newConfig="$(ssh-config-filter "$name" "$ip" <<< ~/.ssh/config)" \
+    
+    ip="$(vm-public-ip $instanceId)" \
+    && cp "$sshPath" "${sshPath}.bak" \
+    && newConfig="$(ssh-config-filter "$name" "$ip" "ec2-instance-id:$instanceId" < "$sshPath")" \
     && tee "$sshPath" <<< "$newConfig"
+}
+
+mds-get-token() {
+    curl -f -s -X PUT "http://169.254.169.254/latest/api/token" \
+        -H "X-aws-ec2-metadata-token-ttl-seconds: 360"
+}
+
+mds-kurl() {
+    local token="$1"
+    shift
+    local path="${2:-"/latest/meta-data/iam/info"}"
+    shift
+
+    [[ -n "$token" ]] || { gen3_log_err "mds-kurl requires token"; return 1; }
+    [[ -n "$path" ]] || { gen3_log_err "mds-kurl requires a path"; return 1; }
+    local -a args=()
+    curl -f -s -H "X-aws-ec2-metadata-token: $token" "http://169.254.169.254/$path" "$@"
+}
+
+caller-identity() {
+    aws sts get-caller-identity
 }
 
 help() {
--- AWS/test/ec2Test.sh
+++ AWS/test/ec2Test.sh
@@ -1,21 +1,20 @@
 test-ec2-ssh-config-filter-empty() {
   local expected="$(cat - <<EOM
+Host dev
+    HostName 10.10.0.5
+    Tag little-trusted:true
+    Tag ec2-instance-id:i-123
+
 Match tagged little-trusted:true
    ServerAliveInterval 120
    ForwardAgent yes
    User ec2-user
    IdentityFile ~/.ssh/id
 
-
-Host dev
-    HostName: 10.10.0.5
-    Tag little-trusted:true
-    Tag aws-id:123
-
 EOM
   )"
   local result
-  result="$(little ec2 ssh-config-filter dev 10.10.0.5 aws-id:123 <<< "")" \
+  result="$(little ec2 ssh-config-filter dev 10.10.0.5 ec2-instance-id:i-123 <<< "")" \
   && [[ "$result" == "$expected" ]]
   because $? "little ec2 ssh-config-filter got expected result: $result"
 }
@@ -23,66 +22,75 @@ EOM
 test-ec2-ssh-config-filter-no-match() {
   local input="$(cat - <<EOM
 Host *
-   IdentityFile ~/.ssh/id_ed25519
-   IdentitiesOnly yes
-
-Match tagged little-trusted:true
-   ServerAliveInterval 120
-   ForwardAgent yes
-   User ec2-user
-   IdentityFile ~/.ssh/id
+    IdentityFile ~/.ssh/id_ed25519
+    IdentitiesOnly yes
 
 Host other
-    HostName: 10.10.0.10
+    HostName 10.10.0.10
     Tag little-trusted:true
-    Tag aws-id:456
+    Tag ec2-instance-id:i-456
+
+Match tagged little-trusted:true
+    ServerAliveInterval 120
+    ForwardAgent yes
+    User ec2-user
+    IdentityFile ~/.ssh/id
 
 EOM
   )"
   local expected="$(cat - <<EOM
-$input
-
 Host dev
-    HostName: 10.10.0.5
+    HostName 10.10.0.5
     Tag little-trusted:true
-    Tag aws-id:123
+    Tag ec2-instance-id:i-123
 
+$input
 EOM
   )"
   local result
-  result="$(little ec2 ssh-config-filter dev 10.10.0.5 aws-id:123 <<< "$input")" \
+  result="$(little ec2 ssh-config-filter dev 10.10.0.5 ec2-instance-id:i-123 <<< "$input")" \
   && [[ "$result" == "$expected" ]]
-  because $? "little ec2 ssh-config-filter got expected result: $result"
+  because $? "little ec2 ssh-config-filter got expected result:\n-----\n$result\n------\n$expected\n----"
 }
 
 test-ec2-ssh-config-filter-overwrite() {
   local input="$(cat - <<EOM
+Host dev
+    HostName 10.10.0.3
+    Tag little-trusted:true
+    Tag ec2-instance-id:i-111
+
 Host *
    IdentityFile ~/.ssh/id_ed25519
    IdentitiesOnly yes
+
+Host other
+    HostName 10.10.0.10
+    Tag little-trusted:true
+    Tag ec2-instance-id:456
 
 Match tagged little-trusted:true
    ServerAliveInterval 120
    ForwardAgent yes
    User ec2-user
    IdentityFile ~/.ssh/id
-
-Host dev
-    HostName: 10.10.0.3
-    Tag little-trusted:true
-    Tag aws-id:111
-
-Host other
-    HostName: 10.10.0.10
-    Tag little-trusted:true
-    Tag aws-id:456
 
 EOM
   )"
   local expected="$(cat - <<EOM
+Host dev
+    HostName 10.10.0.5
+    Tag little-trusted:true
+    Tag ec2-instance-id:i-123
+
 Host *
    IdentityFile ~/.ssh/id_ed25519
    IdentitiesOnly yes
+
+Host other
+    HostName 10.10.0.10
+    Tag little-trusted:true
+    Tag ec2-instance-id:456
 
 Match tagged little-trusted:true
    ServerAliveInterval 120
@@ -90,22 +98,43 @@ Match tagged little-trusted:true
    User ec2-user
    IdentityFile ~/.ssh/id
 
-Host other
-    HostName: 10.10.0.10
-    Tag little-trusted:true
-    Tag aws-id:456
+EOM
+  )"
+  local result
+  result="$(little ec2 ssh-config-filter dev 10.10.0.5 ec2-instance-id:i-123 <<< "$input")" \
+  && [[ "$result" == "$expected" ]]
+  because $? "little ec2 ssh-config-filter got expected result: $result"
+}
 
+test-ec2-vm-name2id() {
+  local sshConfig="$(cat - <<EOM
 Host dev
-    HostName: 10.10.0.5
+    HostName 10.10.0.5
     Tag little-trusted:true
-    Tag aws-id:123
+    Tag ec2-instance-id:i-123
+
+Host *
+   IdentityFile ~/.ssh/id_ed25519
+   IdentitiesOnly yes
+
+Host other
+    HostName 10.10.0.10
+    Tag little-trusted:true
+    Tag ec2-instance-id:i-456
+
+Match tagged little-trusted:true
+   ServerAliveInterval 120
+   ForwardAgent yes
+   User ec2-user
+   IdentityFile ~/.ssh/id
 
 EOM
   )"
   local result
-  result="$(little ec2 ssh-config-filter dev 10.10.0.5 aws-id:123 <<< "$input")" \
-  && [[ "$result" == "$expected" ]]
-  because $? "little ec2 ssh-config-filter got expected result: $result"
+  result="$(little ec2 vm-name2id i-abc)"; because $? "little ec2 vm-name2id i-abc runs ok"
+  [[ "$result" == "i-abc" ]]; because $? "little ec2 vm-name2id i-abc gives expected result: $result"
+  result="$(little ec2 vm-name2id dev "$sshConfig")"; because $? "little ec2 vm-name2id dev runs ok"
+  [[ "$result" == "i-123" ]]; because $? "little ec2 vm-name2id dev gives expected result: $result"
 }
 
 #---
@@ -113,3 +142,4 @@ EOM
 shunit_runtest "test-ec2-ssh-config-filter-empty" "local,ec2"
 shunit_runtest "test-ec2-ssh-config-filter-no-match" "local,ec2"
 shunit_runtest "test-ec2-ssh-config-filter-overwrite" "local,ec2"
+shunit_runtest "test-ec2-vm-name2id" "local,ec2"
